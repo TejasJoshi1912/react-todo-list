5962c25304a11527acf17ddf09c8470253e76fe7
Tejas123-creator-beep <tejasjoshi2k4@gmail.com>
2026-01-20 21:18:58 +0530

    Add ToDo component UI

---
+++ src/ToDo.jsx
@@ -0,0 +1,14 @@
+export default function ToDo(){
+    return(
+        <div>
+        <input placeholder="add a task"></input>
+        <br /><br />
+        <button> Add Task </button>
+        <br /><br /><br />
+
+        <hr />
+        <h4>Tasks Todo</h4>
+        <ul></ul>
+        </div>
+    );
+}; 
