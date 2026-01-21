a52363700fcd5fb98b57d08a08459a4508c7e216
Tejas123-creator-beep <tejasjoshi2k4@gmail.com>
2026-01-21 16:35:31 +0530

    added onclick event on buttin and made two functions that sets the new task under to do . they are addNewTask and updateTodoValue.

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 export default function ToDo(){
+    let [todos,seTodos] = useState (["sample Task"]);
+    let[newTodo,setnewTodo] = useState("");
+
+    let addNewTask = () => {
+        seTodos([...todos,newTodo]);
+        setnewTodo ("")
+    }
+    let updateTodoValue = (event) =>{
+        setnewTodo(event.target.value);
+    } 
     return(
         <div>
-        <input placeholder="add a task"></input>
+        <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
         <br /><br />
-        <button> Add Task </button>
+        <button onClick={addNewTask}> Add Task </button>
         <br /><br /><br />
 
         <hr />
         <h4>Tasks Todo</h4>
-        <ul></ul>
+        <ul>
+            {todos.map((todo) =>(
+                <li>{todo}</li>
+            )
+        )}
+        </ul>
         </div>
     );
 }; 
