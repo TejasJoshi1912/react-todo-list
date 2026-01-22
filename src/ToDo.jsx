06e1dba1f28c1c85b889800a78ad9df8d42144b7
Tejas123-creator-beep <tejasjoshi2k4@gmail.com>
2026-01-22 14:38:40 +0530

    added delete functionality using filter method

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -5,12 +5,21 @@ export default function ToDo(){
     let[newTodo,setnewTodo] = useState("");
 
     let addNewTask = () => {
-        seTodos([...todos,{task:newTodo,id:uuidv4()}]);
+        seTodos((prevTodos)=>{
+            return [...todos,{task:newTodo,id:uuidv4()}];
+        });
         setnewTodo ("")
     }
     let updateTodoValue = (event) =>{
         setnewTodo(event.target.value);
     } 
+    let deleteTodo =  (id) =>{
+
+       seTodos((prevTodos)=>todos.filter((prevTodos)=>prevTodos.id!=id)
+) ;
+       
+
+    }
     return(
         <div>
         <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
@@ -22,7 +31,11 @@ export default function ToDo(){
         <h4>Tasks Todo</h4>
         <ul>
             {todos.map((todo) =>(
-                <li key={todo.id}>{todo.task}</li>
+                <li key={todo.id}>
+                    <span>{todo.task}</span>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button onClick={()=>deleteTodo(todo.id)}>delete</button>
+                    </li>
             )
         )}
         </ul>
